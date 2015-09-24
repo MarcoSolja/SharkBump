3348cbab59b25b58a8a7db7e9bc2d3dd6f70b859
msawyer <msawyer@cccnyc06mxs.sompa.iheart.com>
2015-09-24 14:17:03 -0400

    First Git commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+var diceRoll1: Int = random() % 9
+var diceRoll2: Int = Int(arc4random_uniform(100))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
